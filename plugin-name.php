581aeea12942adb7e9bd109081e6f6d395249b01
Anatoly <48402954+povly@users.noreply.github.com>
2022-01-26 11:09:44 +0300

    fix name

--- plugin-name.php
+++ plugin-name.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:       Todo List
- * Description:       Example Blocks
+ * Plugin Name:       Povly Block
+ * Description:       Povly Blocks
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       todo-list
+ * Text Domain:       povly-block
  *
  * @package           create-block
  */
@@ -20,11 +20,11 @@
  *
  * @see https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/writing-your-first-block-type/
  */
-function create_block_todo_list_block_init() {
+function create_block_povly_block_block_init() {
 	register_block_type( __DIR__ );
 
 }
-add_action( 'init', 'create_block_todo_list_block_init' );
+add_action( 'init', 'create_block_povly_block_block_init' );
 
 function filter_block_categories_when_post_provided( $block_categories, $editor_context ) {
 	if ( ! empty( $editor_context->post ) ) {
@@ -32,7 +32,7 @@ function filter_block_categories_when_post_provided( $block_categories, $editor_
 			$block_categories,
 			array(
 				'slug'  => 'povly',
-				'title' => __( 'Povly', 'todo-list' ),
+				'title' => __( 'Povly', 'povly-block' ),
 				'icon'  => null,
 			)
 		);
@@ -41,14 +41,5 @@ function filter_block_categories_when_post_provided( $block_categories, $editor_
 }
 
 add_filter( 'block_categories_all', 'filter_block_categories_when_post_provided', 10, 2 );
-
-function povlu_register_post_meta() {
-	register_post_meta( 'page', 'povlu_meta_block_field', array(
-		'show_in_rest' => true,
-		'single' => true,
-		'type' => 'string',
-	) );
-}
-add_action( 'init', 'povlu_register_post_meta' );
 
 
