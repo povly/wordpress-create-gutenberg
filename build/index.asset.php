4a3ba46e8cbfe33c71f43542bee47f5a608e5c2e
Anatoly <povly19995@gmail.com>
2022-01-27 13:06:43 +0300

    delete build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3d8d595e3ba216ef7b5e8fd1280aa976');
